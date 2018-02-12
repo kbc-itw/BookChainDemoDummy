dbd0d6ef81d8038d2de3c17105cf1d9c35ba2f58
wamekukyouzin <wamekukyouzin@gmail.com>
2018-02-12 21:44:08 +0900

    qrcode用画面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RentaldummyComponent } from './rentaldummy/rentaldummy.component';
+import { QrcodeComponent } from './qrcode/qrcode.component';
 const routes: Routes = [
   {
     path: '',
     component: RentaldummyComponent
   },
+  {
+    path: 'qrcode',
+    component: QrcodeComponent
+  }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RentaldummyComponent
+    RentaldummyComponent,
+    QrcodeComponent
   ],
   imports: [
     BrowserModule,
